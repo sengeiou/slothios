9aba1a31136d4d5cf1eed0176735a8274a1daf81
YidingW <wangyiding.chinese@gmail.com>
2016-10-20 21:15:37 +0800

    Xcode shit

---
+++ SlothChat/NetworkEngine.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkEngine.swift
+//  SlothChat
+//
+//  Created by 王一丁 on 2016/10/18.
+//  Copyright © 2016年 ssloth.com. All rights reserved.
+//
+
+import UIKit
+
+class NetworkEngine: NSObject {
+
+}
