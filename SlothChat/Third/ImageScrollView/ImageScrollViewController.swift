0bdb58a4871d6c44fccfd8b38b44e1bff3cc994a
Fly <lifeiccl@126.com>
2016-11-04 00:17:04 +0800

    update

--- SlothChat/Third/ImageScrollView/ImageScrollViewController.swift
+++ SlothChat/Third/ImageScrollView/ImageScrollViewController.swift
@@ -66,8 +66,6 @@ class ImageScrollViewController: BaseViewController {
         super.viewDidLoad()
         sentupToolBar()
         sentupView()
-        isShowLikeButton(isShow: !isMyself)
-        isShowDeleteButton(isShow: isMyself)
     }
     
     func sentupToolBar() {
@@ -123,6 +121,10 @@ class ImageScrollViewController: BaseViewController {
         //heart-solid
         likeButton.setImage(UIImage.init(named: "heart-hollow"), for: .normal)
         likeButton.addTarget(self, action: #selector(likeButtonClick), for: .touchUpInside)
+        
+        isShowLikeButton(isShow: !isMyself)
+        isShowDeleteButton(isShow: isMyself)
+        refreshLikeButton()
     }
     
     func isShowDeleteButton(isShow: Bool) {
